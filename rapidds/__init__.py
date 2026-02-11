f197f67f0698a4859dae05f1c4fef667f0d038f3
unknown <joeljobinse@gmail.com>
2026-02-11 19:36:22 +0530

    Add dependencies (pandas, numpy, scikit-learn) and bump to v0.1.1

--- rapidds/__init__.py
+++ rapidds/__init__.py
@@ -2,4 +2,4 @@ from .dataset import Dataset
 
 __all__ = ["Dataset"]
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
---
+++ tests/test_import.py
@@ -0,0 +1,3 @@
+def test_import():
+    import rapidds
+    assert rapidds.__version__ == "0.1.0"
